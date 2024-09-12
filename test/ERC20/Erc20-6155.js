0d4eb255750dad45ca28ca94f86760912eaf10e5
Farhan Yusof <montecristodev0516@gmail.com>
2024-09-12 15:55:59 +1000

    up 12

--- test/ERC20/Erc20-6155.js
+++ test/ERC20/Erc20-6155.js
@@ -292,6 +292,31 @@ contract("XOLE", async accounts => {
         assertPrint("Total to share:", '498495030004550855', await xole.totalRewarded());
     })
 
+    it("John Deposit for 1 weeks, Tom 2 weeks increase unlock time to 4 weeks", async () => {
+
+        await ole.mint(john, toWei(10000));
+        await ole.mint(tom, toWei(10000));
+        await dai.mint(xole.address, toWei(1000));
+        await ole.approve(xole.address, toWei(500), {from: john});
+        await ole.approve(xole.address, toWei(1000), {from: tom});
+        let lastbk = await web3.eth.getBlock('latest');
+        let timeToMove = lastbk.timestamp + (WEEK - lastbk.timestamp % WEEK);
+        m.log("Move time to start of the week", new Date(timeToMove));
+
+        step("John stake 500 2 weeks");
+        await xole.create_lock(toWei(500), timeToMove + 2 * WEEK + 60, {from: john});
+        step("Tom stake 500 2 weeks");
+        lastbk = await web3.eth.getBlock('latest');
+        await xole.create_lock(toWei(500), timeToMove + (2 * WEEK) + 60 * 60, {from: tom});
+        timeToMove = lastbk.timestamp + WEEK;
+        await xole.increase_unlock_time(timeToMove + (4 * WEEK) + 60 * 60, {from: tom});
+
+        step("New reward 1");
+        await xole.convertToSharingToken(toWei(1), 0, daiOLEDexData);
+        assertPrint("Dev Fund:", '498495030004550854', await xole.devFund());
+        assertPrint("Total to share:", '498495030004550855', await xole.totalRewarded());
+    })
+
 
 
   
